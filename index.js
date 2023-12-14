79264aaa40794e5f7a76564bb00e21108b5e5c18
Felipe Lima <felipelimaoliveira20@gmail.com>
2023-12-14 14:52:29 -0300

    finalizando desafio

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-ashjfdgvjhf
+let victory = 120
+let defeat = 5
+let rank
+let rankedResult = rankedBalance(victory,defeat)
+    if(rankedResult<10){
+        rank = "Ferro"
+    }else if(rankedResult>=11 && rankedResult<=20){
+        rank = "Bronze"
+    }else if(rankedResult>=21 && rankedResult<=50){
+        rank = "Prata"
+    }else if(rankedResult>=51 && rankedResult<=80){
+        rank = "Ouro"
+    }else if(rankedResult>=81 && rankedResult<=90){
+        rank = "Diamante"
+    }else if(rankedResult>=91 && rankedResult<=100){
+        rank = "Lendário"
+    }else if(rankedResult>=101){
+        rank = "Imortal"
+    }
+
+    console.log(`O Herói tem de saldo de = ${rankedResult} está no nivel de = ${rank}`)
+
+function rankedBalance(victory, defeat){
+    return victory-defeat
+}
